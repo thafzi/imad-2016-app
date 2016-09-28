61cc84de1fb378008bd4e4bd6edeed35449916ac
thafzi <pvthafsi@gmail.com>
2016-09-28 23:44:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,9 +13,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile('article two');
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
