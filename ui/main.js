0299a26fbf3d963c566be978894e809ab6eec45d
thafzi <pvthafsi@gmail.com>
2016-10-08 09:33:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-var button=document.getElementById("counter")
+var button=document.getElementById("counter");
 var counter=0;
 button.onClick=function(){
     //make request to the counder end point
@@ -10,4 +10,4 @@ button.onClick=function(){
     counter=counter+1;
     var span=document.getElementById("count");
     span.innerHTML=counter.toString();
-}
+};
