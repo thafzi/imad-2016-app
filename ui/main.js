8c76afd9309a4afcbb5880a1bdece6106950e339
thafzi <pvthafsi@gmail.com>
2016-10-08 09:32:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,13 @@
-console.log('Loaded!');
-
-
-//move the madi
-
-var img=documet.getElementByID('madi');
-img=onClick=function(){
-  img.style.marginRight="100px";  
-};
+var button=document.getElementById("counter")
+var counter=0;
+button.onClick=function(){
+    //make request to the counder end point
+    
+    
+    //cspture the response and store in avariable
+    
+    //render the variable in a correct span
+    counter=counter+1;
+    var span=document.getElementById("count");
+    span.innerHTML=counter.toString();
+}
